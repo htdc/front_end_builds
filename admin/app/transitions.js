d7de0f6851d636c202d1fa6ca43fc33a2765bad8
Martin Feckie <mfeckie@gmail.com>
2019-05-29 21:34:00 +0800

    Add transitions

--- admin/app/transitions.js
+++ admin/app/transitions.js
@@ -1,9 +1,8 @@
-export default function(){
-  // Add your transitions here, like:
-  //   this.transition(
-  //     this.fromRoute('people.index'),
-  //     this.toRoute('people.detail'),
-  //     this.use('toLeft'),
-  //     this.reverse('toRight')
-  //   );
+export default function() {
+  this.transition(
+    this.hasClass('animate-sliding-down'),
+    this.toModel(true),
+    this.use('fade', { duration: 300 }),
+    this.reverse('fade', { duration: 300 })
+  );
 }
