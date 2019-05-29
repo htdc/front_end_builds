ccdd422c96bdded9716b18b74051cc52c8188c27
Martin Feckie <mfeckie@gmail.com>
2019-05-29 22:28:36 +0800

    Add HostApp model

---
+++ admin/app/models/host-app.ts
@@ -0,0 +1,19 @@
+import Model from 'ember-data/model'
+import attr from 'ember-data/attr';
+import { computed } from '@ember/object';
+import { capitalize } from '@ember/string';
+
+export default class HostApp extends  Model {
+  @attr('string') name!: string;
+
+  @computed('name')
+  get friendlyName() {
+    return capitalize(this.name.replace('_', ' '));
+  }
+}
+// DO NOT DELETE: this is how TypeScript knows how to look up your models.
+declare module 'ember-data/types/registries/model' {
+  export default interface ModelRegistry {
+    'host-app': HostApp;
+  }
+}
