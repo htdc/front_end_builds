da6594b72cfcfd69152324dae39548586d02c633
Martin Feckie <mfeckie@gmail.com>
2019-05-29 22:25:05 +0800

    Add public key model attrs

--- admin/app/models/public-key.ts
+++ admin/app/models/public-key.ts
@@ -1,9 +1,12 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
 
-export default class PublicKey extends DS.Model.extend({
 
-}) {
-  // normal class body definition here
+export default class PublicKey extends Model {
+  @attr('string') name!: string;
+  @attr('string') pubKey!: string;
+  @attr('string') fingerprint!: string;
+  @attr('date') lastUsedAt?: Date;
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your models.
