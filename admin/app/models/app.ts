d652066e376721408eda623d6d483d7364227c22
Martin Feckie <mfeckie@gmail.com>
2019-05-29 21:51:21 +0800

    Add App model attributes

--- admin/app/models/app.ts
+++ admin/app/models/app.ts
@@ -1,9 +1,23 @@
-import DS from 'ember-data';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { not, sort } from '@ember/object/computed';
+import Build from './build';
 
-export default class App extends DS.Model.extend({
+export default class App extends Model {
+  @attr('string') name!: string;
+  @attr('string') apiKey!: string;
+  @attr('string') locaiton!: string;
+  @attr('boolean') requireManualActivation!: boolean;
+  @not('requireManualActivation') activateNewDeploys!: boolean;
 
-}) {
-  // normal class body definition here
+  buildsSorting = ['createdAt:desc'];
+
+  @sort('builds', 'buildsSorting') orderedBuilds!: Build[];
+
+  @hasMany('build') builds?: Build[];
+
+  @belongsTo('build') liveBuild?: Build;
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your models.
