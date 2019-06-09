79c263cbd6915175e3437cfd98dfc9bfba6d1626
Martin Feckie <mfeckie@gmail.com>
2019-06-09 17:58:43 +1000

    Add app card

---
+++ admin/app/components/app-card/component.ts
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { computed, get } from '@ember/object';
+import { isPresent } from '@ember/utils';
+import App from 'admin/models/app';
+
+type AppArgs = {
+  app: App;
+};
+
+export default class AppCard extends Component<AppArgs> {
+  @computed('app.liveBuild')
+  get titleClass() {
+    const app = get(this.args, 'app');
+    const liveBuild = get(app, 'liveBuild');
+    if (isPresent(liveBuild && liveBuild.id)) {
+      return 'green';
+    }
+    return 'grey';
+  }
+}
