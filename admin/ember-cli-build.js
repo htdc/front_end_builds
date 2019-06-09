57395746840bd60c00d3788dacd8b45703b26ac3
Martin Feckie <mfeckie@gmail.com>
2019-06-09 19:53:23 +1000

    Fix CSS module scoping

--- admin/ember-cli-build.js
+++ admin/ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    cssModules: {
+      headerModules: [
+        'admin/styles/app'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
