2684354853546695be304b7fffe97b92c4fcb2d9
Martin Feckie <mfeckie@gmail.com>
2019-06-10 23:48:57 +1000

    Beginnings of public key pag

--- admin/mirage/factories/public-key.js
+++ admin/mirage/factories/public-key.js
@@ -3,5 +3,6 @@ import faker from 'faker';
 
 export default Factory.extend({
   fingerprint: () => faker.random.alphaNumeric(20).match(/../g).join(':'),
-  name: faker.hacker.phrase
+  name: faker.hacker.phrase,
+  lastUsedAt: faker.date.past
 });
