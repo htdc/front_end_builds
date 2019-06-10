d7ed086acbd077ec2a41a873a2315eb139fd418c
Martin Feckie <mfeckie@gmail.com>
2019-06-10 23:41:44 +1000

    Add mirage factor for public keys

--- admin/mirage/factories/public-key.js
+++ admin/mirage/factories/public-key.js
@@ -1,4 +1,7 @@
 import { Factory } from 'ember-cli-mirage';
+import faker from 'faker';
 
 export default Factory.extend({
+  fingerprint: () => faker.random.alphaNumeric(20).match(/../g).join(':'),
+  name: faker.hacker.phrase
 });
