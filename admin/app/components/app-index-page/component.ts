6476da39d8e27c6cd12a99ed769be8b9e5311d2d
Martin Feckie <mfeckie@gmail.com>
2019-06-10 23:23:14 +1000

    Sort apps

--- admin/app/components/app-index-page/component.ts
+++ admin/app/components/app-index-page/component.ts
@@ -1,5 +1,12 @@
 import Component from '@glimmer/component';
+import { sort } from '@ember/object/computed';
+import App from 'admin/models/app';
 
-export default class AppIndexPage extends Component {
+type IndexArgs = {
+  apps: App[]
+}
 
+export default class AppIndexPage extends Component<IndexArgs> {
+  sortDefinition = ['name:asc'];
+  @sort('args.apps', 'sortDefinition') sortedApps!: App[];
 }
